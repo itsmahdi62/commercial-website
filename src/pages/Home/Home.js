e00b71e48e7364dda6e50ec1cc87aba81da9cd25
Mahdi <amiralmasi021@gmail.com>
2023-04-21 01:19:23 +0330

    Remove additional codes

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -17,16 +17,7 @@ import {AiOutlineArrowDown} from "react-icons/ai"
 const Home = () => {
     const [data,setData] = useState([]);
     const [loading,setLoading] = useState(false); 
-    // const point = 'https://fakestoreapi.com/products'
-    // const [product , setProduct] = useState([
-    //     {id:1 , title:"Nike Triple Black Air Force 1 '07 sneakers" , description: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for", price:1.620 , point:point[1] , imgSrc:{productImage}},
-    //     {id:2 , title:"Nike Triple Black Air Force 1 '07 sneakers" , description: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for", price:1.620 , point:point[2] , imgSrc:{productImage}},
-    //     {id:3 , title:"Nike Triple Black Air Force 1 '07 sneakers" , description: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for", price:1.620 , point:point[0] , imgSrc:{productImage}},
-    //     {id:4 , title:"Nike Triple Black Air Force 1 '07 sneakers" , description: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for", price:1.620 , point:point[4] , imgSrc:{productImage}},
-    // ])
-    // useEffect(()=>{
-    //     const response = axios.get('https://fakestoreapi.com/products')
-    // })   
+    
     useEffect( ()=>{
         axios({
             method:"GET",
