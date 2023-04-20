1247e3fd6560ebd87cc105344f5369a9d8d5bd71
Mahdi <amiralmasi021@gmail.com>
2023-04-21 01:17:26 +0330

    make navbar responsive

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -4,20 +4,27 @@ import "./Navbar.scss";
 //=================== import icons ===================
 import {AiOutlinePlusCircle} from 'react-icons/ai';
 import {BiShoppingBag} from "react-icons/bi"
+import {TbGridDots} from 'react-icons/tb'
 //================== import images ================================
 import logo from "../../assets/vector.png"
 import logo1 from "../../assets/Group 2.png"
 
 const Navbar = () => {
+    const[ navbar , setNavbar] = useState('navbar')
+    const navbarHandler = () => {
+        setNavbar('navbar show')
+    }
+    
     return (
          <div className='header' >
      <div className="logoDiv">
+        <TbGridDots className='mobile' onClick={navbarHandler}/>
         <img src={logo1} alt="" className="logo logoBag" />
         <img src={logo} alt="" className="logo logoText" />
      </div>
 
     {/* ==========================the menuBar and it's options====================== */}
-     <div className='navbar'>
+     <div className={navbar}>
         <ul className="menu">
             <li className="listItem">
                 <a href="/#" className="link">electronic</a>
--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -16,18 +16,30 @@ import {AiOutlineArrowDown} from "react-icons/ai"
 
 const Home = () => {
     const [data,setData] = useState([]);
+    const [loading,setLoading] = useState(false); 
+    // const point = 'https://fakestoreapi.com/products'
+    // const [product , setProduct] = useState([
+    //     {id:1 , title:"Nike Triple Black Air Force 1 '07 sneakers" , description: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for", price:1.620 , point:point[1] , imgSrc:{productImage}},
+    //     {id:2 , title:"Nike Triple Black Air Force 1 '07 sneakers" , description: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for", price:1.620 , point:point[2] , imgSrc:{productImage}},
+    //     {id:3 , title:"Nike Triple Black Air Force 1 '07 sneakers" , description: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for", price:1.620 , point:point[0] , imgSrc:{productImage}},
+    //     {id:4 , title:"Nike Triple Black Air Force 1 '07 sneakers" , description: "Lorem ipsum is placeholder text commonly used in the graphic, print, and publishing industries for", price:1.620 , point:point[4] , imgSrc:{productImage}},
+    // ])
+    // useEffect(()=>{
+    //     const response = axios.get('https://fakestoreapi.com/products')
+    // })   
     useEffect( ()=>{
         axios({
             method:"GET",
             url: `https://fakestoreapi.com/products`
         }).then(response =>{
             setData(response.data)
-            
+            console.log(response.data)
         }).catch(e=>console.log(e))
         
     })
     return ( 
     <div className="home">
+
         <div className="banner">
             <img src={banner} alt="" />
         </div>
@@ -46,8 +58,6 @@ const Home = () => {
                     <AiOutlineArrowDown />
                 </div>
             </div>
-            {console.log(data)}
-
         <div className="products flex">
            {
                 data.map((item) =>{
@@ -56,7 +66,7 @@ const Home = () => {
                         title={item.title}
                         description={item.description}
                         price={item.price}
-                        point={item.rating.rate}
+                        point={item.point}
                         img={item.imgSrc}
                     />)
                 })
